db775c1bc1b8da11b607cf0bd3c8a9df2a15a0a8
adrianix1 <adrian.panczyk@wp.pl>
2024-05-21 21:03:00 +0200

    Create ball and its movement.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 sc = Screen()
 sc.setup(width=800, height=600)
 sc.bgcolor("black")
 sc.title("Breakout Game")
+sc.tracer(0)
 
 paddle = Paddle()
+ball = Ball()
 
 sc.listen()
 sc.onkey(paddle.move_r, "Right")
 sc.onkey(paddle.move_l, "Left")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    sc.update()
+    ball.ball_moving()
 
 sc.exitonclick()
